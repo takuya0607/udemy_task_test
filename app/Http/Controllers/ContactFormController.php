a62e6065973fe7454a6b01ac7a72f28b8babe1d3
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2020-10-17 15:40:56 +0900

    DBにデータを保存できるようにしました

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\ContactForm;
 class ContactFormController extends Controller
 {
     /**
@@ -36,7 +37,19 @@ class ContactFormController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $contact = new ContactForm;
+
+      $contact->your_name = $request->input('your_name');
+      $contact->title = $request->input('title');
+      $contact->email = $request->input('email');
+      $contact->url = $request->input('url');
+      $contact->gender = $request->input('gender');
+      $contact->age = $request->input('age');
+      $contact->contact = $request->input('contact');
+
+      $contact->save();
+
+      return redirect('contact/index');      // dd($your_name,$title);
     }
 
     /**
