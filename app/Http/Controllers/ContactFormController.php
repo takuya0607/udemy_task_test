7adf01f60c092dfab04b0b1a683ef943867ff19a
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2020-10-21 19:50:17 +0900

    ページネーション の実装を行いました

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -23,7 +23,7 @@ class ContactFormController extends Controller
         $contacts = DB::table('contact_forms')
         ->select('id','your_name','title','created_at')
         ->orderBy('created_at','desc')
-        ->get();
+        ->paginate(20);
 
         // dd($contacts);
 
