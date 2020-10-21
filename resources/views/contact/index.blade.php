674d52620622654572b146759cc21aa7f7e677ad
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2020-10-21 20:15:10 +0900

    検索機能の実装を行いました

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -18,6 +18,11 @@
                     </button>
                     </form>
 
+                    <form method="GET" action="{{ route('contact.index') }}" class="form-inline">
+                      <input class="form-control mr-sm-2"  name="search" type="search" placeholder="検索" aria-label="Search">
+                      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索する</button>
+                    </form>
+
                     <table class="table">
                       <thead>
                         <tr>
